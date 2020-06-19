790ae0fe328a0306ac8b83b2265f70f2b4c87caf
Chris Sachs <chris@swim.ai>
2020-06-19 12:25:43 -0700

    Update copyrights

--- swim-core-js/@swim/build/main/Target.ts
+++ swim-core-js/@swim/build/main/Target.ts
@@ -1,4 +1,4 @@
-// Copyright 2015-2020 SWIM.AI inc.
+// Copyright 2015-2020 Swim inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -587,6 +587,8 @@ export class Target {
           exports: inputChunk.exports,
           isEntry: inputChunk.isEntry,
           isDynamicEntry: inputChunk.isDynamicEntry,
+          isImplicitEntry: false,
+          implicitlyLoadedBefore: [],
         };
         bundle.output.push(outputChunk);
       } else {
